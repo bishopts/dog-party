a3e3998a28458a153da3eb8a8ac72596d1dd4616
tyler seay <seay.tyler@gmail.com>
2018-11-27 13:34:34 -0500

    intial commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const prefix       = require('gulp-autoprefixer');
+const browserSync  = require('browser-sync').create();
+const imagemin     = require('gulp-imagemin');
+
+function css() {
+  return gulp.src('src/assets/sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(prefix({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('src/assets/css'))
+    .pipe(browserSync.stream());
+};
+
+function serve() {
+  browserSync.init({
+    server: "./src"
+  })
+};
+
+function copy() {
+  return gulp.src([
+    'src/**',
+    '!src/assets/sass/**',
+    '!src/images/**'
+  ])
+    .pipe(gulp.dest('dist/'));
+};
+
+function images() {
+  return gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+}
+
+exports.default = serve;
+exports.build = gulp.series(images, copy);
+
+gulp.watch('src/assets/sass/**', { events: 'all' }, css);
+gulp.watch('src/*.html', { events: 'all' }, browserSync.reload);
